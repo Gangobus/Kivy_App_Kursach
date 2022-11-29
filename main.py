ba9cdfe75588d65dcbf75fd40e08cb71029c4af2
”Sasha <sasha1900sasha@gmail.com>
2022-11-30 00:38:39 +0300

    +font

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
-from kivymd.icon_definitions import md_icons
 
 from libs.screens.home_page import HomePage
-
 
 class KivyApp(MDApp):
     def build(self):
@@ -13,8 +11,7 @@ class KivyApp(MDApp):
         return HomePage()
     def load_all_kv_files(self):
         Builder.load_file('libs/screens/home_page.kv')
-
-
+        Builder.load_file('libs/components/appbar.kv')
 
 if __name__ == "__main__":
     KivyApp().run()
