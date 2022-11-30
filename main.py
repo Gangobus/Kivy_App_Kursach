5b58e7baf821fb09aa1464b159b9df3df00e67fe
”Sasha <sasha1900sasha@gmail.com>
2022-11-30 15:08:28 +0300

    StartPage

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from kivymd.app import MDApp
+from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle, Color
+from kivy.uix.button import ButtonBehavior
+
 
 from libs.screens.home_page import HomePage
+from libs.screens.start_page import StartPage
 
 class KivyApp(MDApp):
     def build(self):
-        Window.size = [300, 600]
+        Window.size = [360, 800]
         self.load_all_kv_files()
-        return HomePage()
+        return StartPage()
     def load_all_kv_files(self):
         Builder.load_file('libs/screens/home_page.kv')
         Builder.load_file('libs/components/appbar.kv')
+        Builder.load_file('libs/screens/start_page.kv')
 
 if __name__ == "__main__":
     KivyApp().run()
