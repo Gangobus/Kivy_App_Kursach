066360db365462d75400a7b1fd5bb74be2308f9c
”Sasha <sasha1900sasha@gmail.com>
2022-12-01 15:42:52 +0300

    StartPage_v1

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ class StartPage(Screen):
 class HomePage(Screen):
     pass
 
+class LessonsPage(Screen):
+    pass
+
+
 class MyApp(MDApp):
     def build(self):
         Window.size = [360, 780]
         Builder.load_file("libs/screens/start_page.kv")
         Builder.load_file("libs/screens/home_page.kv")
+        Builder.load_file("libs/screens/lessons_page.kv")
 
         sm = ScreenManager()
         sm.add_widget(StartPage(name="GO"))
         sm.add_widget(HomePage(name="Lessons"))
         sm.add_widget(HomePage(name="Tests"))
+        sm.add_widget(LessonsPage(name="Back"))
         return sm
 
 MyApp().run()
