5427a42fdb1a6c893d03378fd8d94f6b81471049
”Sasha <sasha1900sasha@gmail.com>
2022-12-05 01:47:22 +0300

    StartPage_v1

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+
 
 class StartPage(Screen):
     pass
 
+
 class HomePage(Screen):
     pass
 
+
 class LessonsPage(Screen):
+    pass
+
+class TestsPage(Screen):
     pass
 
 
 class MyApp(MDApp):
     def build(self):
-        Window.size = [360, 780]
+        Window.size = [300,600]
         Builder.load_file("libs/screens/start_page.kv")
-        Builder.load_file("libs/screens/home_page.kv")
+        #Builder.load_file("libs/screens/home_page.kv")
         Builder.load_file("libs/screens/lessons_page.kv")
+        Builder.load_file("libs/screens/tests_page.kv")
 
         sm = ScreenManager()
-        sm.add_widget(StartPage(name="GO"))
-        sm.add_widget(HomePage(name="Lessons"))
-        sm.add_widget(HomePage(name="Tests"))
-        sm.add_widget(LessonsPage(name="Back"))
+        sm.add_widget(StartPage(name="Go"))
+        #sm.add_widget(HomePage(name="Homepg"))
+        #sm.add_widget(HomePage(name="Homepg"))
+        sm.add_widget(LessonsPage(name="Lessons"))
+        sm.add_widget(TestsPage(name="Tests"))
         return sm
+
 
 MyApp().run()
