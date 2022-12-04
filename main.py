787c5e63833fd773c7dea1152e477274eddeb401
”Sasha <sasha1900sasha@gmail.com>
2022-12-05 02:57:24 +0300

    botton bar

--- main.py
+++ main.py
@@ -7,32 +7,34 @@ from kivymd.app import MDApp
 class StartPage(Screen):
     pass
 
-
-class HomePage(Screen):
-    pass
-
-
 class LessonsPage(Screen):
     pass
 
 class TestsPage(Screen):
     pass
 
+class GamePage(Screen):
+    pass
+
+class VideosPage(Screen):
+    pass
 
 class MyApp(MDApp):
     def build(self):
-        Window.size = [300,600]
+        Window.size = [360,720]
         Builder.load_file("libs/screens/start_page.kv")
-        #Builder.load_file("libs/screens/home_page.kv")
         Builder.load_file("libs/screens/lessons_page.kv")
         Builder.load_file("libs/screens/tests_page.kv")
+        Builder.load_file("libs/screens/game_page.kv")
+        Builder.load_file("libs/screens/videos_page.kv")
+
 
         sm = ScreenManager()
         sm.add_widget(StartPage(name="Go"))
-        #sm.add_widget(HomePage(name="Homepg"))
-        #sm.add_widget(HomePage(name="Homepg"))
         sm.add_widget(LessonsPage(name="Lessons"))
         sm.add_widget(TestsPage(name="Tests"))
+        sm.add_widget(GamePage(name="Game"))
+        sm.add_widget(VideosPage(name="Videos"))
         return sm
 
 
